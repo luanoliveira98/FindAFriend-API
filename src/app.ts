4c2d4934606b9b3b85c31eecebfd120358a8c0c6
Luan Oliveira <luanoliveira17@hotmail.com>
2024-10-08 19:33:30 -0300

    feat: initializing app and server

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import fastify from 'fastify'
+import { env } from 'process'
+import { ZodError } from 'zod'
+
+export const app = fastify()
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
